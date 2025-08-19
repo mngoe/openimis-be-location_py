fdb74953f92b2f3306b0aa42c4946f120a9893fb
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-08-19 08:38:10 +0100

    Update 0022_migrationjsonExt_1517.py: to fix ci

--- location/migrations/0022_migrationjsonExt_1517.py
+++ location/migrations/0022_migrationjsonExt_1517.py
@@ -15,4 +15,9 @@ class Migration(migrations.Migration):
             name='json_ext',
             field=models.JSONField(blank=True, db_column='JsonExt', null=True),
         ),
+        migrations.AddField(
+            model_name='healthfacility',
+            name='json_ext',
+            field=models.JSONField(blank=True, db_column='JsonExt', null=True),
+        ),
     ]
