752268edd7a8e99109621f692d1a08db081d37a7
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-08-19 08:25:37 +0100

    Update 0022_migrationjsonExt_1517.py

--- location/migrations/0022_migrationjsonExt_1517.py
+++ location/migrations/0022_migrationjsonExt_1517.py
@@ -10,9 +10,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.AddField(
-        #     model_name='location',
-        #     name='json_ext',
-        #     field=models.JSONField(blank=True, db_column='JsonExt', null=True),
-        # ),
+        migrations.AddField(
+            model_name='location',
+            name='json_ext',
+            field=models.JSONField(blank=True, db_column='JsonExt', null=True),
+        ),
     ]
