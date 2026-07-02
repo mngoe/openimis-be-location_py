3feaee94565417892f61059f2df9d3eb2f69d36b
kevel choumene <k.choumene@cloudfacile.com>
2026-07-02 13:30:36 +0100

    feat: debug

--- location/models.py
+++ location/models.py
@@ -165,6 +165,7 @@ class LocationManager(models.Manager):
             q_allowed_location = Q((f"{prefix}__in", self.allowed(user.id, loc_types))) | Q((f"{prefix}__isnull", True))
 
             if queryset:
+                print("OK.... ", queryset.filter(q_allowed_location))
                 return queryset.filter(q_allowed_location)
             else:
                 print("Juste ", q_allowed_location)
@@ -199,6 +200,7 @@ class LocationManager(models.Manager):
             q_allowed_location = Q((f"{prefix}__in", self.allowed(user.id, loc_types))) | Q((f"{prefix}__isnull", True))
 
             if queryset is not None:
+                print("OK.... ", queryset.filter(q_allowed_location))
                 return queryset.filter(q_allowed_location)
             else:
                 print("Juste ", q_allowed_location)
